97f49f151e49f0091ac11745970aefc7c5e67bce
kurovu146 <kurobakaito146@gmail.com>
2026-02-01 20:29:06 +0700

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'RealFake FC',
+        short_name: 'RealFake FC',
+        description: 'Official Portal of RealFake FC - Stats, Fixtures & Squad',
+        theme_color: '#38003c',
+        background_color: '#38003c',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: '/',
   resolve: {
     alias: {
